edbb5e9e6c2686aa5e85b78792393ffb72cb0855
karlderkaefer <karlderkaefer@users.noreply.github.com>
2021-06-05 11:28:37 +0200

    fix: add default region for login
    closes #16

--- action/login.go
+++ action/login.go
@@ -40,7 +40,7 @@ func (updater *CredUpdater) init() {
 	cfg, err := config.LoadDefaultConfig(
 		context.TODO(),
 		config.WithSharedConfigProfile(updater.sourceProfile),
-		//config.WithRegion("eu-central-1"),
+		config.WithRegion("eu-central-1"),
 	)
 	if err != nil {
 		log.Fatal(err)
